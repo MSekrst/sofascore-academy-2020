79347ce1dd8d9973912018dcb85048d43b59fbac
MSekrst <matija.sekrst13@gmail.com>
2020-03-11 14:16:02 +0100

    Added Homework 2 solutions

--- homework/02/src/solutions.js
+++ homework/02/src/solutions.js
@@ -2,99 +2,173 @@ module.exports = {
   /**
    * Returns an empty object without prototype. There is object creation type that creates object without prototype
    */
-  createPrototypelessObject() {},
+  createPrototypelessObject() {
+    return Object.create(null)
+  },
 
   /**
    * Returns an object with prototype set to given `proto`.
    * @param {Object} proto Prototype object
    */
-  createObjectWithPrototype(proto) {},
+  createObjectWithPrototype(proto) {
+    return Object.create(proto)
+  },
 
   /**
    * Returns an object with `value` property set to the given `value` and `getValue` method.
    * Be careful, if `value` changes, `getValue` should return changed `value`.
    * @param {any} value
    */
-  createObjectWithMethod(value) {},
+  createObjectWithMethod(value) {
+    return {
+      value,
+      getValue: function() {
+        return this.value
+      },
+    }
+  },
 
   /**
    * Returns an object with the `getValue` and `setValue` methods, having `value` hidden from the outside.
    */
-  createEncapsulatedObject() {},
+  createEncapsulatedObject() {
+    return (function() {
+      let value = 0
+
+      return {
+        setValue: newValue => {
+          value = newValue
+        },
+        getValue: () => value,
+      }
+    })()
+  },
 
   /**
    * Returns the shallow copy of the given `obj`. HINT: This **operator** will be used later.
    * @param {Object} obj
    */
-  shallowCopy(obj) {},
+  shallowCopy(obj) {
+    return { ...obj }
+  },
 
   /**
    * Returns the deep copy of the given `obj`.
    * @param {Object} obj
    */
-  deepCopy(obj) {},
+  deepCopy(obj) {
+    return JSON.parse(JSON.stringify(obj))
+  },
 
   /**
    * Returns an array containing 2 elements which are
    * loosely equal, but strictly unequal.
    */
-  looselyTrue() {},
+  looselyTrue() {
+    return ['0', 0]
+  },
 
   /**
    * Returns a string that is loosely equal to boolean `true`. This one is tricky :)
    */
-  stringLooselyEqualToTrue() {},
+  stringLooselyEqualToTrue() {
+    return '1'
+  },
 
   /**
    * Returns correct sum of a and b.
    */
-  safeSum(a, b) {},
+  safeSum(a, b) {
+    return Number(a) + Number(b)
+  },
 
   /**
    * Returns formatted string for the given date.
    * Format should be `{day}-{month}-{fullYear}` (all numbers).
    * @param {Date} date
    */
-  formatDate(date) {},
+  formatDate(date) {
+    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+  },
 
   /**
    * Sorts the given `numberArray` in ascending order.
    * Use array `.sort` method. Sort is done in place so there is no need to return anything.
    * @param {number[]} numberArray
    */
-  sortNumberArray(numberArray) {},
+  sortNumberArray(numberArray) {
+    numberArray.sort((a, b) => a - b)
+  },
 
   /**
    * Multiplies all the elements in the array by 2 _in place_
    * (edits the given array) and returns it.
    * @param {number[]} numberArray
    */
-  multiplyArrayByTwo(numberArray) {},
+  multiplyArrayByTwo(numberArray) {
+    for (let i = 0; i < numberArray.length; i += 1) {
+      numberArray[i] *= 2
+    }
+
+    return numberArray
+  },
 
   /**
    * Multiplies all the elements in the array by 2 and returns them
    * in a new array.
    * @param numberArray
    */
-  multiplyArrayByTwoNew(numberArray) {},
+  multiplyArrayByTwoNew(numberArray) {
+    return numberArray.map(num => num * 2)
+  },
 
   /**
    * Create two classes: `Person` and `Programmer`. `Programmer` class extends `Person`.
    * Person class has `name` property (set via constructor) and `getName` method (calls `callGetName` with name`).
    * Programmer class has `language` property provided to constructor (and `name` inherited from `Person`) and `getLanguage` method (calls `callGetLanguage` with `language`)
-   * Return object with created classes, `return { Person, Programmer }`.
    *
    * NOTE: class methods should use `bind`, function expression syntax won't work here because code isn't transpiled.
    *
    * @param {Function} callGetName
    * @param {Function} callGetLanguage
    */
-  classInheritance(callGetName, callGetLanguage) {},
+  classInheritance(callGetName, callGetLanguage) {
+    class Person {
+      constructor(name) {
+        this.name = name
+
+        this.getName = this.getName.bind(this)
+      }
+
+      getName() {
+        callGetName(this.name)
+      }
+    }
+
+    class Programmer extends Person {
+      constructor(name, language) {
+        super(name)
+
+        this.language = language
+
+        this.getLanguage = this.getLanguage.bind(this)
+      }
+
+      getLanguage() {
+        callGetLanguage(this.language)
+      }
+    }
+
+    return {
+      Person,
+      Programmer,
+    }
+  },
 
   /**
-   * EXTRA CREDIT TASK -> Closure trick with async. Async is not important here and has nothing to do with the solution.
+   * EXTRA CREDIT TASK -> Closure trick with async (we will learn async next time). Async is not important here and has nothing to do with the solution.
    *
-   * **This is variant of probably most common "big firm" interview question with closures.**
+   * **This is probably most common "big firm" interview question with closures.**
    *
    * This task has more easier solutions (e.g. using `let` instead of `var`), but desired solutions included Closures.
    *
@@ -103,10 +177,15 @@ module.exports = {
    * @param {Function} consumer
    */
   timeoutIncrement(consumer) {
+    const factory = i => () => {
+      consumer(i)
+    }
+
     for (var i = 1; i <= 3; i += 1) {
-      setTimeout(() => {
-        /* your function goes here, or instead of this function */
-      }, 1000)
+      // setTimeout(() => {
+      //   /* your function goes here, or instead of this function */
+      // }, 1000)
+      setTimeout(factory(i), 1000)
     }
   },
 }
