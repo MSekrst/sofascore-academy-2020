1f8aa160dca74b828fcc2095ac6b20e8ee48af6f
Matija Šekrst <matija.sekrst13@gmail.com>
2020-03-11 09:22:21 +0100

    Lesson 02 (#6)
    * Lesson 02 materials
    
    * Change deploy scripts
    
    * Fixed typo
    
    * Changed scripts
    
    * Modified package.json for merge

---
+++ lessons/02-files/types.js
@@ -0,0 +1,7 @@
+console.log(typeof 2) // number
+console.log(typeof 'SFA') // string
+console.log(typeof { foo: 'bar' }) // object
+console.log(typeof [1, 2, 3]) // object -> not array!
+console.log(typeof (() => {})) // function
+console.log(typeof null) // object -> JScript bug!
+console.log(typeof NaN) // number -> Not a number is a number :)
